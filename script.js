bcbbb2b3e99ef02ae01d794ae9e5cd8df0b7b26d
Saurabh Patel <saurabh.patel0894@gmail.com>
2022-11-25 03:35:53 +0530

    portfolio update

--- script.js
+++ script.js
@@ -1,77 +1,101 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    // sticky navbar on scroll script
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId);
+  
+    if (toggle && nav) {
+      toggle.addEventListener("click", () => {
+        nav.classList.toggle("show");
+      });
     }
+  };
+  showMenu("nav-toggle", "nav-menu");
+  
+  /*===== ACTIVE AND REMOVE MENU =====*/
+  const navLinks = document.querySelectorAll(".nav-link");
+  const sections = document.querySelectorAll(".section");
+  
+  window.addEventListener("scroll", () => {
+    let current = '';
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (scrollY >= sectionTop - 390) {
+        current = section.getAttribute('id');
+      }
+    })
+  
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.classList.contains(current)) {
+        link.classList.add('active');
+      }
+    })
+  })
+  
 
-    // scroll-up button show/hide script
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
+  const navMenu = document.getElementById("nav-menu");
+ 
+  navLinks.forEach((n) => n.addEventListener("click", () => { navMenu.classList.remove("show") }));
+  
+  /*===== COPY Email =====*/
+  const copy = document.getElementById("copy");
+  copy.addEventListener("click", () => {
+    navigator.clipboard.writeText("saurabhpatel0894@gmail.com");
+    copy.innerHTML = "copied";
+    setTimeout(() => {
+      copy.innerHTML = null;
+    }, 1000);
+  });
+  
+  /*===== SCROLL REVEAL ANIMATION =====*/
+  const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+  });
+  
+  /*SCROLL HOME*/
+  sr.reveal(".home-title", {});
+  sr.reveal(".button", { delay: 100 });
+  sr.reveal(".home-img", { delay: 200 });
+  sr.reveal(".home-social-icon", { interval: 200 });
+  
+  /*SCROLL ABOUT*/
+  sr.reveal(".about-img", {});
+  sr.reveal(".about-subtitle", { delay: 200 });
+  sr.reveal(".about-text", { delay: 200 });
+  
+  /*SCROLL SKILLS*/
+  sr.reveal(".skills-subtitle", {});
+  sr.reveal(".skills-text", {});
+  sr.reveal(".skills-data", { interval: 100 });
+  // sr.reveal(".skills-img", { delay: 600 });
+  
+  /*SCROLL projects*/
+  sr.reveal(".project-img", { interval: 200 });
+  
+  /*SCROLL CONTACT*/
+    // sr.reveal(".contact-input", { interval: 200 });
+  
+    
+  
+  
+    var messageArr = ["Java Backend Developer", "Frontend Developer", "Backend Developer"];
+    var textPosition = 0;
+    var speed = 200;
+  
+    typewriter = () => {
+      // for(let i = 0; i < messageArr.length; i++) {
+      document.querySelector("#jobTitle").innerHTML = messageArr[0].substring(0, textPosition)  ;
+      if(textPosition ++  != messageArr[0].length)
+          setTimeout(typewriter, speed)
     }
-  });
-
-  // slide-up script
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    // removing smooth scroll on slide-up button click
-    $("html").css("scrollBehavior", "auto");
-  });
-
-  $(".navbar .menu li a").click(function () {
-    // applying again smooth scroll on menu items click
-    $("html").css("scrollBehavior", "smooth");
-  });
-
-  // toggle menu/navbar script
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
-
-  // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Back-End Developer", "Dreamer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  var typed = new Typed(".typing-2", {
-    strings: ["Back-End Developer", "Dreamer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  // owl carousel script
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
-  });
-});
-
-
-GitHubCalendar(".calendar", "Saurabhpatel0894");
-GitHubCalendar(".calendar", "Saurabhpatel0894", { responsive: true });
+  
+  
+    window.addEventListener("load" , typewriter);
+  
+    GitHubCalendar(".calendar", "Saurabhpatel0894");
+    // or enable responsive functionality                
+    GitHubCalendar(".calendar", "Saurabhpatel0894", { responsive: true });
